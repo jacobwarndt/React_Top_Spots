f5d6d3223646484f83a6c73d4c9db791fd9e5ca5
JacobArndt <jacobwarndt@gmail.com>
2025-05-29 16:03:14 -0400

    Refactor TopSpots component to render a list of TopSpot components with proper layout

--- src/TopSpots.jsx
+++ src/TopSpots.jsx
@@ -1,11 +1,18 @@
-import TopSpot from "./TopSpot";
+import React from 'react';
+import TopSpot from './TopSpot';
 
-const TopSpots = ({ spots }) => {
-    return (
-        <>
-            {/* Add your component here */}
-        </>
-    );
-};
+const TopSpots = ({ spots }) => (
+  <div data-testid="topspots" className="row">
+    {spots.map((spot) => (
+      <div key={spot.id} className="col-md-4 mb-4">
+        <TopSpot
+          name={spot.name}
+          description={spot.description}
+          location={spot.location}
+        />
+      </div>
+    ))}
+  </div>
+);
 
 export default TopSpots;
