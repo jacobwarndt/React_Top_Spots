be81800d1b4f118fedb5875ee32920aeb3ad8475
JacobArndt <jacobwarndt@gmail.com>
2025-05-29 16:03:07 -0400

    Refactor App component to use functional component syntax and integrate API call for top spots data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import TopSpots from './TopSpots';
 
-function App() {
-  // Add your code here
+const App = () => {
+  const [topspots, setTopspots] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://ccc.helloworldbox.com/items/top_spots')
+      .then(res => setTopspots(res.data.data));
+  }, []);
 
   return (
-    <>
-      {/* Add your code here */}
+    <div className="container mt-5">
+      <h1>San Diego Top Spots</h1>
+      <p>A list of the top 30 places to see in San Diego, California.</p>
+      <TopSpots spots={topspots} />
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
